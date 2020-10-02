32b864396b51fe8373144fcf3497ea60a35f9844
Akecel <axel.rayer@gmail.com>
2020-10-02 19:51:34 +0200

    🗃  : update migrations

--- database/migrations/database.sql
+++ database/migrations/database.sql
@@ -10,10 +10,11 @@ Version 1
 
 CREATE TABLE "user"
 (
-    "id"         SERIAL PRIMARY KEY,
+    "id"        int(11) NOT NULL,
     "last_name"  varchar(50) NULL,
     "first_name" varchar(50) NULL,
     "email"      varchar(50) NOT NULL,
-    "password"   varchar(255) NOT NULL
-);
+    "password"   varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
