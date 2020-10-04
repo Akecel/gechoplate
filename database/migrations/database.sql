4cd2264c94ac284dfe8681ae0198badc50b0ce5f
Akecel <axel.rayer@gmail.com>
2020-10-04 23:06:04 +0200

    🌱  : add seeding in init dump SQL

--- database/migrations/database.sql
+++ database/migrations/database.sql
@@ -8,6 +8,8 @@ Version 1
 *********************************************************************
 */
 
+/*Schema for the table `user` */
+
 CREATE TABLE `user` (
     `id`         int(11) NOT NULL,
     `last_name`  varchar(50) NULL,
@@ -17,3 +19,8 @@ CREATE TABLE `user` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`last_name`,`first_name`,`email`,`password`) values 
+(1,'Doe','John','john.doe@gechoplate.com',
+'$2y$12$IL71JzMwtV1uMbGRB3hGhe1mGmLrBbKlnB2x4n0RwA0Px3Vx6WyiG'); /* <-- This is 'password' in Bcrypt for User testing */
